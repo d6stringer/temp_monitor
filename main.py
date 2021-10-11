746a7207d26441400b0c3634c21ae7edc5512a44
Daniel Woodson <s.dwoodson@gmail.com>
2021-10-11 23:11:13 +0000

    init commit

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+Bay Materials, Inc.
+Written by: Daniel Woodson
+Init: 11 Oct 2021
+
+The main purpose of this script is to monitor the output of our temperature loggers and send out some kind of alert if they detect an out of spec condition. They are currently writting to ubidots. 
+
+
+"""
